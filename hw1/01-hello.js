b2bbd9f46729372aa45fde083f937d6415d85d3d
ahmad <ahmad.agah@pdx.edu>
2023-10-02 23:29:14 -0700

    first commit

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,11 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+}
+
+function loadPage(event) {
+  console.log(
+    "My name is Ahmad, I am taking CS563 this quarter, I have never experience web development before I love swimming and working out at gym.In my free time I love playing Video Games and I love contributing to open source projects in the internet."
+  );
 }
